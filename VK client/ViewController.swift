6a545c8de596ba670c86bfedb752647e883a60fc
vidyakov <sashavidyakov2000@gmail.com>
2020-01-16 16:59:06 +0300

    Добавил 3 ДЗ

--- VK client/ViewController.swift
+++ VK client/ViewController.swift
@@ -10,18 +10,56 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var loginTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        // Проверяем данные
+        let checkResult = checkUserData()
+        
+        // Если данные не верны, покажем ошибку
+        if !checkResult {
+            showLoginError()
+        }
+        
+        // Вернем результат
+        return checkResult
+    }
+    
+    func checkUserData() -> Bool {
+        guard let login = loginTextField.text,
+            let password = passwordTextField.text else { return false }
+        
+        if login == "admin" && password == "123456" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func showLoginError() {
+        // Создаем контроллер
+        let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
+        // Создаем кнопку для UIAlertController
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        // Добавляем кнопку на UIAlertController
+        alter.addAction(action)
+        // Показываем UIAlertController
+        present(alter, animated: true, completion: nil)
+    }
+
+    
+
     @IBAction func loginBtnPresed(_ sender: UIButton) {
-        print(loginTextField.text ?? "")
-        print(passwordTextField.text ?? "")
+        if loginTextField.text == "root" && passwordTextField.text == "1234" {
+            print("Ok")
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
+
 
